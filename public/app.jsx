1483d8ec9955c8a3eb3a597cc7c118f8d58b28ae
Dunia Goncalves <dgoncalves@everquote.com>
2018-01-29 09:14:05 -0500

    Nested Components

--- public/app.jsx
+++ public/app.jsx
@@ -1,23 +1,35 @@
 var GreeterMessage = React.createClass({
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+
     return (
       <div>
-      <h1>Some h1</h1>
-      <p>some paragraph</p>
-    </div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      </div>
     );    
   }
 });
 
 var GreeterForm = React.createClass({
+  onFormSubmit: function (e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      this.props.onNewName(name);
+    }
+  },
+
   render: function() {
     return (
-      <div>
-        <form>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
-      </div>
+      <form onSubmit={this.onFormSubmit}>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
     );
   }
 });
@@ -34,46 +46,28 @@ var Greeter = React.createClass({
       name: this.props.name
     };
   },
-  onBtnClick: function (e) {
-    e.preventDefault();
-
-    var nameRef = this.refs.name;
-    var name = nameRef.value;
-    nameRef.value = '';
-
-    if (typeof name === 'string' && name.length > 0) {
-       this.setState({ name: name });
-    }
-
-   
+ handleNewName: function (name) {
+    this.setState({
+      name: name
+    });
   },
   render: function() {
     var name = this.state.name;
     var message = this.props.message;
+    
     return (
       <div>
-        <h1>Hello {name}!</h1>
-        <p>{message}</p>
-
-        <GreeterMessage/>
-
-        <form onSubmit={this.onBtnClick}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
-
-        <GreeterForm/>
-
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewName={this.handleNewName}/>
       </div>
     );
   }
 });
 
 var firstName = 'Dunia';
-var message = 'Hey, What\'s up?'
 
 ReactDOM.render(
-  <Greeter name={firstName} message={message}/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
 
